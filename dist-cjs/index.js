6451c2fd921a3ac27c0ea44125a0e501b0e67793
coseficq <121393108+coseficq@users.noreply.github.com>
2023-02-03 11:55:29 +0300

    Add files via upload

--- dist-cjs/index.js
+++ dist-cjs/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
-tslib_1.__exportStar(require("./fromTokenFile"), exports);
-tslib_1.__exportStar(require("./fromWebToken"), exports);
+tslib_1.__exportStar(require("./lib/aws/partition"), exports);
+tslib_1.__exportStar(require("./resolveEndpoint"), exports);
+tslib_1.__exportStar(require("./types"), exports);
