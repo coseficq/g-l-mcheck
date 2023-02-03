8ae578c20ac03ff0792b18b4a7f1e7072d88dfa6
coseficq <121393108+coseficq@users.noreply.github.com>
2023-02-03 11:49:49 +0300

    Add files via upload

--- dist-es/index.js
+++ dist-es/index.js
@@ -1,6 +1,1 @@
-export * from "./STS";
-export * from "./STSClient";
-export * from "./commands";
-export * from "./defaultRoleAssumers";
-export * from "./models";
-export { STSServiceException } from "./models/STSServiceException";
+export * from "./fromIni";
